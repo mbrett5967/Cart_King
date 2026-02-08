9c830e3971482314a41bad9ced756300e51af728
mbrett5967 <matthewbrett@hotmail.co.uk>
2026-02-08 15:30:55 +0000

    Created Buisness page, View and Controller

---
+++ Controllers/BusinessController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Cart_King.Controllers
+{
+    
+    public class BusinessController : Controller
+    {
+        private readonly ILogger<BusinessController> _logger;
+
+        public BusinessController(ILogger<BusinessController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
