1e11c8635a0c6a57f0b6f72fc77fd0413419765d
mbrett5967 <matthewbrett@hotmail.co.uk>
2026-04-10 14:55:41 +0100

    Added Controller action "Logout()" for profile sign out button

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,19 +4,34 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Identity;
 
 namespace Cart_King.Controllers
 {
-    
     public class AccountController : Controller
     {
-       
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+
+        // Opens User account page
         public IActionResult OpenDashboard()
         {
             return View("Index");
         }
+
+
+        // Signout Button
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
-       
 }
