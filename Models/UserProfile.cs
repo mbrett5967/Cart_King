bf8bb5e3d3b5251de71112867cc2c5f94001ad9e
mbrett5967 <matthewbrett@hotmail.co.uk>
2026-04-16 19:18:15 +0100

    added model data and modified attributes

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Cart_King.Models
 {
@@ -12,15 +13,18 @@ namespace Cart_King.Models
     public int Id { get; set; }
 
     //connects profile to the login account
-    [Required]
-    public string? IdentityUserId { get; set; } 
+       [Required]
+    public string? IdentityUserId { get; set; }
+
+    [ForeignKey("IdentityUserId")]
+    public IdentityUser? User { get; set; }
     
 
     // User Info
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? DeliveryAddress { get; set; }
+    public string? AddressLine1 { get; set; }
     public string? City { get; set; }
-    public string? PostCode { get; set; }
+    public string? Postcode { get; set; }
     }
 }
