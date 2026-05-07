3d36d3e1d8fd8e3ff5a29699b9e640031118294c
mbrett5967 <matthewbrett@hotmail.co.uk>
2026-05-07 20:00:53 +0100

    new db update for userprofile

---
+++ Migrations/20260430170019_AddedMobileNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cart_King.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMobileNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MobileNumber",
+                table: "UserProfiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "UserProfiles");
+        }
+    }
+}
---
+++ Migrations/20260430183533_UpdateUserProfile.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cart_King.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MobileNumber",
+                table: "UserProfiles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MobileNumber",
+                table: "UserProfiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
